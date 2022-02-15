ff1d9ea1c8eb4ddbb8616258f13866ba1cab2510
Jaikishan roy <jaikishanroy@gmail.com>
2022-02-15 12:20:14 +0530

    added user constructor

--- src/main/java/com/inventory/backend/inventory/models/User.java
+++ src/main/java/com/inventory/backend/inventory/models/User.java
@@ -38,4 +38,13 @@ public class User {
             inverseJoinColumns = @JoinColumn(name ="role_id"))
     private Set<Role> roles = new HashSet<>();
 
+    public User(){
+
+    }
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email =email;
+        this.password =password;
+    }
+
 }
